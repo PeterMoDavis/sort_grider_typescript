f2452f8b4a589390b51dbb467bd97e92e7f7383d
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-24 16:00:18 -0400

    added Type Guards

--- build/index.js
+++ build/index.js
@@ -1,5 +1,33 @@
 "use strict";
-console.log("hi there.");
-var logSomething = function () {
-    console.log("hello");
-};
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection instanceof LinkedList) {
+                }
+                //all of this only works if collection is number[]
+                //If collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    //collection === number[]
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                //If collection is a string , do this logic instead:
+                //logic to compare characters in a string
+                if (typeof this.collection === "string") {
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
